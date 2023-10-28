0b524480a8bc084d252a1aebb08751990a05d5fb
Tuan <minhtamtele1@gmail.com>
2023-10-28 17:59:37 +0700

    Update DB and Front-end cus

--- DoAn_FrameWork/Controllers/ProductController.cs
+++ DoAn_FrameWork/Controllers/ProductController.cs
@@ -7,22 +7,23 @@ namespace DoAn_FrameWork.Controllers
 {
     public class ProductController : Controller
     {
-        //private readonly TechnoShop_DBContext _context;
-        //public ProductController(TechnoShop_DBContext context) { 
-        //    _context = context;
-        //}
+        private readonly TechnoShop_DBContext _context;
+        public ProductController(TechnoShop_DBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        //public IActionResult Product_Detail(int id)
-        //{
-        //    var product = _context.Products.Include(x => x.CategoryId).FirstOrDefault(x => x.ProductId == id);
-        //    if (product == null)
-        //    {
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View();
-        //}
+        public IActionResult Product_Detail(int id)
+        {
+            //var product = _context.Products.Include(x => x.CategoryId).FirstOrDefault(x => x.ProductId == id);
+            //if (product == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+            return View();
+        }
     }
 }
--- DoAn_FrameWork/Program.cs
+++ DoAn_FrameWork/Program.cs
@@ -35,22 +35,32 @@
 //});
 
 //app.Run();
+using DoAn_FrameWork.Data;
+using DoAn_FrameWork.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+var connectionString = builder.Configuration.GetConnectionString("TechnoShop_DB");
+builder.Services.AddDbContext<TechnoShop_DBContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+builder.Services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.All}));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 IConfiguration configuration = app.Configuration;
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 
@@ -67,16 +77,16 @@ app.MapControllerRoute(
 
 app.Run();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
 
-app.UseRouting();
+//app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-app.Run();
+//app.Run();
