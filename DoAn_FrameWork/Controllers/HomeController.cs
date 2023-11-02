c24ff8f22c4c4a8c47428ed77ba15f6dddddfdad
Tuan <minhtamtele1@gmail.com>
2023-11-02 10:07:26 +0700

    Fix UI Homepage

--- DoAn_FrameWork/Controllers/HomeController.cs
+++ DoAn_FrameWork/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using DoAn_FrameWork.Models.Authentication;
 using Microsoft.EntityFrameworkCore;
+using X.PagedList;
 
 namespace DoAn_FrameWork.Controllers
 {
@@ -16,9 +17,36 @@ namespace DoAn_FrameWork.Controllers
             _logger = logger;
         }
         //[Authentication]
-        public IActionResult Index()
+        public IActionResult Index(int? page, String SearchString = "")
         {
-            return View();
+            //if (SearchString != "")
+            //{
+            //    var sanPham = _context.Products.Include(s => s.Category)
+            //        .Where(x => x.ProductName.ToUpper().Contains(SearchString.ToUpper()));
+            //    return View(sanPham.ToList());
+            //}
+            try
+            {
+                var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+                var pageSize = 8;
+                var lsProducts = _context.Products
+                    .AsNoTracking()
+                    .OrderByDescending(x => x.ProductId);
+                if (SearchString != "")
+                {
+                    var sanPham = _context.Products.Include(s => s.Category)
+                    .Where(x => x.ProductName.ToUpper().Contains(SearchString.ToUpper()));
+                    PagedList<Product> searchProduct = new PagedList<Product>(sanPham.ToList(), pageNumber, pageSize);
+                    return View(searchProduct);
+                }
+                PagedList<Product> models = new PagedList<Product>(lsProducts, pageNumber, pageSize);
+                ViewBag.CurrentPage = page;
+                return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
         public IActionResult About()
         {
