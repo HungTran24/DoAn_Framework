8ac38ab2d8ef3c357237f1852ecd9a9459eccb6b
NgocTuan <doanngoctuan27@gmail.com>
2023-12-06 17:10:39 +0700

    Tính năng Đổi mật khẩu ở trang thông tin

--- DoAn_FrameWork/Controllers/AccountController.cs
+++ DoAn_FrameWork/Controllers/AccountController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAn_FrameWork.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAn_FrameWork.Controllers
 {
     public class AccountController : Controller
     {
+        TechnoShop_DBContext _context = new TechnoShop_DBContext();
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Customer user)
+        {
+            var existingUser = _context.Customers.FirstOrDefault(x => x.CustomerEmail.Equals(user.CustomerEmail));
+            if (existingUser != null)
+            {
+                string newPassword = user.RePassword;
+
+                existingUser.Password = newPassword;
+                _context.Update(existingUser);
+                _context.SaveChanges();
+
+                return View();
+            }
             return View();
         }
     }
