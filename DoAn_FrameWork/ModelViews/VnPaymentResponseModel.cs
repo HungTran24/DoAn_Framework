7eadbca66cfacc3fc21bc28cafa13e32174b7430
HungTran24 <tranquocphong123452401@gmail.com>
2023-12-27 00:03:35 +0700

    Thêm thanh toán, vnpay,

---
+++ DoAn_FrameWork/ModelViews/VnPaymentResponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DoAn_FrameWork.ModelViews
+{
+    public class VnPaymentResponseModel
+    {
+        public bool Success { get; set; }
+        public string PaymentMethod { get; set; }
+        public string OrderDescription { get; set; }
+        public string OrderId { get; set; }
+        public string PaymentId { get; set; }
+        public string TransactionId { get; set; }
+        public string Token { get; set; }
+        public string VnPayResponseCode { get; set; }
+    }
+    public class VnPaymentRequestModel
+    {
+        public string Name { get; set; }
+        public double Amount { get; set; }
+
+        public int OrderId { get; set; }
+
+        public string OrderDescription { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
