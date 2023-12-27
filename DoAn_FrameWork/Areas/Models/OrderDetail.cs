77f67c1db2889f78c2f4eb9ef9da45161a7fd877
NgocTuan <doanngoctuan27@gmail.com>
2023-12-27 12:02:28 +0700

    fix thông báo

---
+++ DoAn_FrameWork/Areas/Models/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoAn_FrameWork.Areas.Models
+{
+    public partial class OrderDetail
+    {
+        public int OrderDetailsId { get; set; }
+        public int? OrderId { get; set; }
+        public int? ProductId { get; set; }
+        public int? ProductSalesQuantity { get; set; }
+
+        public virtual Order? Order { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
