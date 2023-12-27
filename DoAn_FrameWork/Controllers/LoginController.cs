17eabfa51c904ff35f30b00c1c107141cc63fdcc
NgocTuan <doanngoctuan27@gmail.com>
2023-12-27 10:10:50 +0700

    fix thông báo

--- DoAn_FrameWork/Controllers/LoginController.cs
+++ DoAn_FrameWork/Controllers/LoginController.cs
@@ -98,6 +98,7 @@ public class LoginController : Controller
             else
             {
                 ViewBag.RegisterFail = "Tài khoản đã tồn tại!";
+                //ok
             }
         }
         return View();
